515700a68b9de8ccaa88850f4cf2894b05fab568
aya-cs22 <aya786930@gmail.com>
2025-02-15 22:31:12 +0200

    plus one

--- array/66-Plus-One.js
+++ array/66-Plus-One.js
@@ -1,10 +1,20 @@
-//https://www.geeksforgeeks.org/how-to-convert-a-number-into-array-in-javascript/
 /**
  * @param {number[]} digits
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let num = Number(digits.join('')) + 1
-    let arr = num.toString().split('').map(Number);
-    return (arr)
+    const arr = []
+    let str =  digits.join("") ;
+    // console.log(str);
+    let numbers = BigInt(str) +1n;
+    // console.log(numbers);
+    numbers = String(numbers);
+    // console.log(numbers);
+    for(let i = 0; i < numbers.length; i++){
+        arr.push(Number(numbers[i]));
+    }
+    return arr;
+  
+    
 };
+console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
