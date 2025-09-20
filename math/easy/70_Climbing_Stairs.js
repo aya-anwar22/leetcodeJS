54510c8b6c6ead0e97696fcea216c68e61636128
Abdul <522501@oi.edu.eg>
2025-09-20 10:26:14 +0300

    climb Stairs

---
+++ math/easy/70_Climbing_Stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let arr = [1,2]
+    for(let i = 2; i <= n; i++){
+        arr.push(Number(arr[i - 1] + arr[i - 2] ))
+    }
+    return arr[n - 1]
+    
+};
+console.log(climbStairs(7))
