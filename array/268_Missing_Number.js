0866d1e992d1d2bc7dbf910d616eae8163104b6f
Abdul <522501@oi.edu.eg>
2025-10-03 12:18:15 +0300

    missing Number

---
+++ array/268_Missing_Number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let result = nums.length
+    for(let i = 0; i <= nums.length; i++){
+        if (!nums.includes(i)){
+            return i
+        }
+
+    }    
+};
+console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
